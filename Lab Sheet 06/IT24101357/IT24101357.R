bb6e44ff4a0cb966fcc14affe23b263a07b6cbde
IT24101357 <IT24101357@my.sliit.lk>
2025-09-17 09:19:52 +0530

    Add files via upload

---
+++ Lab Sheet 06/IT24101357/IT24101357.R
@@ -0,0 +1,20 @@
+getwd()
+setwd("C:\\Users\\LENOVO\\Desktop\\IT24101357")
+
+#Question 1
+#Binomial Distribution
+#Here,random variable X has binomial distribution with n=50 and p=0.85
+
+#Question 2
+pbinom(46,50,0.85,lower.tail = FALSE)
+
+#Part 2
+#Question 1
+#Number of customers calls per hour
+
+#Question 2
+#poisson distribution
+#Here ,random variable X has poisson distribution with lambda=12
+
+#Question 3
+dpois(15,12)
